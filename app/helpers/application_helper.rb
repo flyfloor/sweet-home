a51d86aae91aec4034cc2ef6420f7399be69d4cf
jerryshew <jerryshew@gmail.com>
2013-12-18 00:01:46 +0800

    update time refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,25 @@
 module ApplicationHelper
+  require "digest/md5"
 
   def title page_title
-  	base_title = "Lacuna's_Blog"
-  	if page_title.empty?
-  		base_title
-  	else
-  		page_title
-  	end
+    base_title = "Lacuna's_Blog"
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
   end
 
   def html_view text
-  	text = Kramdown::Document.new(text).to_html.gsub("\n", "\r")
+    text = Kramdown::Document.new(text).to_html.gsub("\n", "\r")
   end
 
   def updated_time target
     target.updated_at.localtime.to_s(:db) unless target.blank?
+  end
+
+  def gravatar email
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest email.strip.downcase}"
   end
 
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,2 @@
 module CommentsHelper
-	require "digest/md5"
-
-	def gravatar email
-		"http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest email.strip.downcase}"
-	end
-
 end
