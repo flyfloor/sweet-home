bdbb6d49a6275abc4221be9f498b7950ba635754
jerryshew <jerryshew@gmail.com>
2014-01-02 23:56:02 +0800

    refatoring

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,7 @@ class Picture < ActiveRecord::Base
 	has_and_belongs_to_many :tags
   attr_accessible :description, :filename, :title
   has_many :comments, dependent: :destroy
+
+  attr_accessible :avatar
+  has_attached_file :avatar :default_url => "/uploads/images/missing.png"
 end
